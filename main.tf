3c1674ed938aba973f38463835eec077f56afcfe
Sean Sundberg <seansund@us.ibm.com>
2021-08-14 22:48:00 -0500

    Replaces setup-binaries script with submodule (#4)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 locals {
-  bin_dir                = "${path.cwd}/bin"
+  bin_dir = module.setup_clis.bin_dir
   yaml_dir               = "${path.cwd}/.tmp/artifactory"
   ingress_host           = "artifactory-${var.namespace}.${var.cluster_ingress_hostname}"
   ingress_url            = "https://${local.ingress_host}"
@@ -131,19 +131,11 @@ locals {
   values_file = "values-${var.server_name}.yaml"
 }
 
-resource null_resource setup_binaries {
-  provisioner "local-exec" {
-    command = "${path.module}/scripts/setup-binaries.sh"
-
-    environment = {
-      BIN_DIR = local.bin_dir
-    }
-  }
+module setup_clis {
+  source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
 }
 
 resource null_resource create_yaml {
-  depends_on = [null_resource.setup_binaries]
-
   provisioner "local-exec" {
     command = "${path.module}/scripts/create-yaml.sh '${local.yaml_dir}' '${local.values_file}'"
 
@@ -191,7 +183,7 @@ resource null_resource setup_gitops {
   depends_on = [null_resource.create_yaml, module.service_account, module.config_service_account]
 
   provisioner "local-exec" {
-    command = "$(command -v igc || command -v ${local.bin_dir}/igc) gitops-module '${local.name}' -n '${var.namespace}' --contentDir '${local.yaml_dir}' --serverName '${var.server_name}' -l '${local.layer}' --valueFiles 'values.yaml,${local.values_file}'"
+    command = "${local.bin_dir}/igc gitops-module '${local.name}' -n '${var.namespace}' --contentDir '${local.yaml_dir}' --serverName '${var.server_name}' -l '${local.layer}' --valueFiles 'values.yaml,${local.values_file}'"
 
     environment = {
       GIT_CREDENTIALS = yamlencode(var.git_credentials)
