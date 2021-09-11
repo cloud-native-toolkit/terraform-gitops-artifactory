6d3f8893f6618e4f945e76568c587d5e0eb4a254
Sean Sundberg <seansund@us.ibm.com>
2021-09-11 00:20:38 -0500

    Makes cluster dependency optional (only necessary for IKS) (#5)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-artifactory.tf
+++ test/stages/stage2-artifactory.tf
@@ -4,11 +4,5 @@ module "gitops_artifactory" {
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
   namespace = module.gitops_namespace.name
-//  cluster_ingress_hostname = module.dev_cluster.platform.ingress
-//  cluster_type             = module.dev_cluster.platform.type_code
-//  tls_secret_name          = module.dev_cluster.platform.tls_secret
-  cluster_ingress_hostname = "test"
-  cluster_type             = "ocp4"
-  tls_secret_name          = "test"
   server_name = module.gitops.server_name
 }
