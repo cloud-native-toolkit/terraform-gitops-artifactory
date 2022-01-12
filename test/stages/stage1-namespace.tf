d927761504560b19180d85947ba70924c88734fd
Sean Sundberg <seansund@us.ibm.com>
2022-01-12 14:24:09 -0600

    Updates test variables
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-namespace.tf
+++ test/stages/stage1-namespace.tf
@@ -4,5 +4,10 @@ module "gitops_namespace" {
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
   name = var.namespace
-  server_name = module.gitops.server_name
+}
+
+resource null_resource write_namespace {
+  provisioner "local-exec" {
+    command = "echo -n '${module.gitops_namespace.name}' > .namespace"
+  }
 }
