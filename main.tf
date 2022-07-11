98eb3143b2bb67fe5fc1745a833acfce3766ef12
Ramya Raghuveera <ramragh1@in.ibm.com>
2022-07-11 12:33:42 +0530

    Fixed typo
    Signed-off-by: Ramya Raghuveera <ramragh1@in.ibm.com>

--- main.tf
+++ main.tf
@@ -183,7 +183,7 @@ module "config_service_account" {
 }
 
 module setup_group_scc {
-  depends_on = [module.service_account,module.config_service_accoun]
+  depends_on = [module.service_account,module.config_service_account]
 
   source = "github.com/cloud-native-toolkit/terraform-gitops-sccs.git"
 
