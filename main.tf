d9e61aaf0b223122931402678845a5a098cfe90e
Ramya Raghuveera <ramragh1@in.ibm.com>
2022-07-13 11:27:47 +0530

    Changing the uid back to 0
    Signed-off-by: Ramya Raghuveera <ramragh1@in.ibm.com>

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ locals {
         storageClassName = ""
         size = "5Gi"
       }
-      uid = 1000760000
+      uid = 0
     }
     ingress = {
       enabled = false
@@ -51,7 +51,7 @@ locals {
         storageClassName = var.storage_class
         size = "5Gi"
       }
-      uid = 1000760000
+      uid = 0
     }
     ingress = {
       enabled = var.cluster_type == "kubernetes"
