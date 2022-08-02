37d8ef36a17127a30cd220db04677417d0c578ce
Ramya Raghuveera <ramragh1@in.ibm.com>
2022-08-02 16:40:19 +0530

    Updates for using gitea for gitops repo
    Signed-off-by: Ramya Raghuveera <ramragh1@in.ibm.com>

--- test/stages/variables.tf
+++ test/stages/variables.tf
@@ -80,3 +80,23 @@ variable "kubeseal_namespace" {
 
 variable "cp_entitlement_key" {
 }
+variable "gitea_username" {
+  type = string
+  description = "The username for the instance"
+  default = "gitea-admin"
+}
+variable "gitea_password" {
+  type = string
+  description = "The password for the instance"
+  default = ""
+}
+variable "gitea_instance_name" {
+  type = string
+  description = "The name for the instance"
+  default = "gitea"
+}
+variable "gitea_namespace_name" {
+  type = string
+  description = "The value that should be used for the namespace"
+  default = "gitea"
+}
